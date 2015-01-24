f82836294b027cf0011048b9d6eeee881ee50c55
Tigran Bayburtsyan <tigran.bayburtsyan@gmail.com>
2015-01-24 15:13:34 +0400

    flickr API added and some combined structs

--- src/oneday/one_day_of_earth.go
+++ src/oneday/one_day_of_earth.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"APIs/instagram"
+	"APIs/flickr"
+	//	"APIs/instagram"
 	"fmt"
 	"time"
 )
 
 func main() {
-	images, err := instagram.SearchImages("37.7624499", "-122.4602593", int(time.Now().Unix()-86000), 5000)
+	images, err := flickr.SearchImages("37.7624499", "-122.4602593", int(time.Now().Unix()-86000), 20)
 	if err != nil {
 		fmt.Println(err.Message())
 	}
